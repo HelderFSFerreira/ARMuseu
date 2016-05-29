94fe01f516a783823a7529c5b32378ea30adbf11
HelderFSFerreira <woounn@gmail.com>
2016-05-29 17:07:06 +0100

    Agora é possivel ver obras

--- app/src/main/java/com/example/helde/armuseu/ObraActivity.java
+++ app/src/main/java/com/example/helde/armuseu/ObraActivity.java
@@ -10,24 +10,12 @@ import java.util.ArrayList;
  * Created by Nelson on 24/05/2016.
  */
 public class ObraActivity extends AppCompatActivity {
+    Obra o = null;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.obra_layout);
 
-        ListView listaObras = (ListView) findViewById(R.id.listaObras);
-        ArrayList<Obra> ArrayDadosObra = new ArrayList<Obra>();
-        Obra obras;
-
-
-        obras = new Obra("MonaLisa", "Uma obra muita fantastica!! Venha aprecisar!!",getResources().getDrawable(R.drawable.m),1);
-        ArrayDadosObra.add(obras);
-        obras = new Obra("MonaDeslisa", "Uma obra muita fantastica!! Venha aprecisar!!",getResources().getDrawable(R.drawable.m2),1);
-        ArrayDadosObra.add(obras);
-        obras = new Obra("MonaLisa", "Uma obra muita fantastica!! Venha aprecisar!!",getResources().getDrawable(R.drawable.m),2);
-        ArrayDadosObra.add(obras);
-
-        ObraAdapter adapter = new ObraAdapter(this, ArrayDadosObra);
-
-        listaObras.setAdapter(adapter);
+        o = (Obra) getIntent().getSerializableExtra("Obra");
+        System.out.println(o.getNome());
     }
 }
