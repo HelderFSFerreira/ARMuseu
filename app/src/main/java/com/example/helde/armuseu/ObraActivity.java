bd6592986d5479b5740480749638d1612ec1e267
Nelson Rodrigues <nelsonrs@ipvc.pt>
2016-07-17 18:29:59 +0100

    -mudanças nelso

--- app/src/main/java/com/example/helde/armuseu/ObraActivity.java
+++ app/src/main/java/com/example/helde/armuseu/ObraActivity.java
@@ -2,7 +2,9 @@ package com.example.helde.armuseu;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -11,6 +13,11 @@ import java.util.ArrayList;
  */
 public class ObraActivity extends AppCompatActivity {
     Obra o = null;
+
+    TextView titulo;
+    TextView descricao;
+    ImageView img;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.obra_layout);
@@ -18,6 +25,15 @@ public class ObraActivity extends AppCompatActivity {
         o = (Obra) getIntent().getSerializableExtra("Obra");
         System.out.println(o.getNome());
 
+        titulo = (TextView) findViewById(R.id.nomedaobra);
+        titulo.setText(o.getNome());
+        System.out.println(o.getNome());
+
+        descricao = (TextView) findViewById(R.id.descricaoobra);
+        descricao.setText(o.getDescricao());
+
+        img = (ImageView) findViewById(R.id.museuimagem);
+        //img.setImageDrawable(m.getImagem());
 
     }
 }
